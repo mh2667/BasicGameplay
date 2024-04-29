49b4011baae88e25fabc3fab8e091a0697dd5b71
Maxwell Hernandez <hernandezm3@roverkids.org>
2024-04-29 13:39:05 -0400

    Completed 2.3 Step 4

--- BasicGameplay/Assets/Scripts/SpawnManager.cs
+++ BasicGameplay/Assets/Scripts/SpawnManager.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject[] animalPrefabs;
-    
+    private float spawnRangeX = 20;
+    private float spawnPosZ = 20;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,8 +21,9 @@ public class SpawnManager : MonoBehaviour
         {
 
             int animalIndex = Random.Range(0, animalPrefabs.Length);
+            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
 
-            Instantiate(animalPrefabs[animalIndex], new Vector3(0, 0, 20), animalPrefabs[animalIndex].transform.rotation);
+            Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
         }
     }
 }
